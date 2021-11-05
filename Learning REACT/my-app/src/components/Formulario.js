d01a9324ac676c042d46190ba2d2eb5e36f05b1a
Martin Corredor <dev.martincorredor@gmail.com>
2021-11-05 11:09:46 -0500

    |UPDATED| Add validations to form with react-hook-form

--- Learning REACT/my-app/src/components/Formulario.js
+++ Learning REACT/my-app/src/components/Formulario.js
@@ -1,6 +1,62 @@
-import React from 'react';
-import { Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
+const FormCrud = () => {
+  const { register, errors, handleSubmit } = useForm();
+
+  const [entradas, setentradas] = useState([]);
+
+  const procesarFormulario = (data, e) => {
+    console.log(data);
+    setentradas([...entradas, data]);
+    // limpiar campos
+    e.target.reset();
+  };
+
+  return (
+    <Fragment>
+      <h1>FORM</h1>
+      <form onSubmit={handleSubmit(procesarFormulario)}>
+        <input
+          name="titulo"
+          ref={register({
+            required: { value: true, message: 'Ingrese un nombre' },
+          })}
+          className="form-control my-2"
+          placeholder="Ingrese título"
+        ></input>
+        <span className="text-danger text-small d-block mb-2">
+          {errors?.titulo?.message}
+        </span>
+        <input
+          name="descripcion"
+          ref={register({
+            required: { value: true, message: 'Ingrese descripción' },
+          })}
+          className="form-control my-2"
+          placeholder="Ingrese descripción"
+        ></input>
+        <span className="text-danger text-small d-block mb-2">
+          {errors?.descripcion?.message}
+        </span>
+        <button type="submit" className="btn btn-primary">
+          Agregar
+        </button>
+      </form>
+      <ul className="mt-2">
+        {entradas.map((item, index) => (
+          <li key={index}>
+            {item.titulo} - {item.descripcion}
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default FormCrud;
+
+/*
 const Form = () => {
   const [data, setdata] = useState({
     firstName: '',
@@ -19,6 +75,7 @@ const Form = () => {
     console.log(data.firstName + ' ' + data.name)
   }
 
+
   return (
     <Fragment>
       <h1>Form</h1>
@@ -30,6 +87,7 @@ const Form = () => {
             type="text"
             name="firstName"
             onChange={handleInputChance}
+            
           ></input>
         </div>
         <div className="col-md-3">
@@ -52,3 +110,4 @@ const Form = () => {
 };
 
 export default Form;
+*/
