4e215d051a91a24b3a7d4cfd08ca085d4ab12158
Martin Corredor <dev.martincorredor@gmail.com>
2021-11-26 16:01:53 -0500

    Tic tac toe

---
+++ Learning REACT/tic-tac-toe/src/components/Board.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Square from './Square';
+
+function Board() {
+  const status = 'Next player: X';
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {Square(0)}
+        {Square(1)}
+        {Square(2)}
+      </div>
+      <div className="board-row">
+        {Square(3)}
+        {Square(4)}
+        {Square(5)}
+      </div>
+      <div className="board-row">
+        {Square(6)}
+        {Square(7)}
+        {Square(8)}
+      </div>
+    </div>
+  );
+}
+
+export default Board;
