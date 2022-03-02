81613e053bff8451def630412bc6a559b3248c5a
Martin Corredor <dev.martincorredor@gmail.com>
2022-03-02 14:56:41 -0500

    |ADDED| Leran how to do for cicle and if declaration with vue

---
+++ Learning VUE/firstSteps/index.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    titulo: 'Hello world with Vue',
+    frutas: [
+        {nombre:'pera', cantidad: 10 },
+        {nombre: 'manzana', cantidad: 0},
+        {nombre: 'platano', cantidad: 8}
+    ]
+  },
+});
