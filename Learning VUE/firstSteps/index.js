6cdc05c55e0f6a2907665c4283ceb6d3de4bccd4
Martin Corredor <dev.martincorredor@gmail.com>
2022-03-02 16:49:32 -0500

    |ADDED| Class N°3. keyup, v-model and computed :o

--- Learning VUE/firstSteps/index.js
+++ Learning VUE/firstSteps/index.js
@@ -14,6 +14,7 @@ const app = new Vue({
 */
 
 /*================= Second Class ===================== */
+/*
 const app = new Vue({
     el: '#app',
     data: {
@@ -33,3 +34,38 @@ const app = new Vue({
         }
     }
   });
+*/
+
+/*================= Second Class ===================== */
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    title: 'Hello world with Vue',
+    fruits: [
+      { name: 'pera', quantity: 10 },
+      { name: 'manzana', quantity: 0 },
+      { name: 'platano', quantity: 8 },
+    ],
+    newFruit: '',
+    total: 0
+  },
+  methods: {
+    addFruit() {
+      this.fruits.push({
+        name: this.newFruit,
+        quantity: 0,
+      });
+      this.newFruit = '';
+    },
+  },
+  computed: {
+      sumFruits(){
+          this.total = 0;
+          for(fruit of this.fruits){
+              this.total = this.total + fruit.quantity;
+          }
+          return this.total;
+      }
+  }
+});
